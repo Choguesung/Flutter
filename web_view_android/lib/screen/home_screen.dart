93432331a07a1907bb60b1dcf66f57d75064068c
LSH <121619411+Choguesung@users.noreply.github.com>
2023-02-02 14:05:22 +0900

    123

---
+++ web_view_android/lib/screen/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Code Factory'),
+      ),
+      body: WebView(
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
+
+
+
