f252dc2e621a4f1e879cca8dc31c545dc95d63fb
LSH <121619411+Choguesung@users.noreply.github.com>
2023-02-03 17:00:46 +0900

    heloww

--- web_view_android/lib/screen/home_screen.dart
+++ web_view_android/lib/screen/home_screen.dart
@@ -3,16 +3,37 @@ import 'package:webview_flutter/webview_flutter.dart';
 
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  WebViewController? controller;
+  final homeurl = 'https://blog.codefactory.ai';
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.orange,
         title: Text('Code Factory'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: (){
+              if(controller == null){
+                return;
+              }
+              controller!.loadUrl(homeurl);
+            },
+            icon: Icon(
+              Icons.home,
+           ),
+          ),
+        ],
       ),
       body: WebView(
-        initialUrl: 'https://blog.codefactory.ai',
+        onWebViewCreated: (WebViewController controller){
+          this.controller = controller;
+        },
+        initialUrl: homeurl,
         javascriptMode: JavascriptMode.unrestricted,
       ),
     );
