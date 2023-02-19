769495c37e7a8ab092ddefae8b87d2aec4a2fd9a
LSH <121619411+Choguesung@users.noreply.github.com>
2023-02-19 16:06:28 +0900

    23123123

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,13 +11,94 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  static final LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
+  static final CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hoee'),
+      appBar: renderAppBar(),
+      body: Column(
+        children: [
+          _CustomGoogleMap(initialPosition: initialPosition),
+          _ChoolCheckButton(),
+        ],
       ),
+    );
+  }
 
+  Future<String> checkPermission() async{
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled){
+      return '위치 서비스를 활성화 해주세용용';
+    }
+
+    LocationPermission checkPermission = await Geolocator.checkPermission();
+
+    if(checkPermission == LocationPermission.denied){
+      checkPermission = await Geolocator.requestPermission();
+
+      if(checkPermission == LocationPermission.denied){
+        return '위치 권한을 허가 해주세용';
+      }
+    }
+
+    if(checkPermission == LocationPermission.deniedForever){
+      return '앱의 위치 권한을 설정에서 허가 해주세요';
+    }
+
+    return '위치 권한을 허가해주세용';
+  }
+
+  AppBar renderAppBar() {
+    return AppBar(
+      title: Text(
+        '오늘도 출근',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+  );
+}
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+
+  const _CustomGoogleMap({
+    required this.initialPosition,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text('출근'),
     );
   }
 }
