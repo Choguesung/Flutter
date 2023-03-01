dace345cb2fd77b26c971842695c67b432c65ec4
Choguesung <tkdgh1135@naver.com>
2023-03-01 17:15:28 +0900

    hello

--- calendar_scheduler/lib/screen/home_screen.dart
+++ calendar_scheduler/lib/screen/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:calendar_scheduler/component/schedule_bottom_sheet.dart';
+import 'package:calendar_scheduler/component/schedule_card.dart';
 import 'package:calendar_scheduler/component/today_banner.dart';
+import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import '../component/calendar.dart';
 
@@ -16,9 +19,11 @@ class _HomeScreenState extends State<HomeScreen> {
     DateTime.now().day,
   );
   DateTime focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: renderFloatingActionButton(),
       body: SafeArea(
         child: Column(
           children: [
@@ -34,16 +39,67 @@ class _HomeScreenState extends State<HomeScreen> {
               selectedDay: selectedDay,
               scheduleCount: 3,
             ),
+            SizedBox(
+              height: 8.0,
+            ),
+            _ScheduleList(),
           ],
         ),
       ),
     );
   }
 
-  onDaySelected (DateTime selectedDay, DateTime focusedDay) {
+  FloatingActionButton renderFloatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          isScrollControlled: true,
+          builder: (_){
+            return ScheduleBottomSheet();
+          }
+        );
+      },
+      backgroundColor: PRIMARY_COLOR,
+      child: Icon(
+        Icons.add,
+      ),
+    );
+  }
+
+  onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
       this.selectedDay = selectedDay;
       this.focusedDay = selectedDay;
     });
+  }
+}
+
+class _ScheduleList extends StatelessWidget {
+  const _ScheduleList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 8.0,
+        ),
+        child: ListView.separated(
+          itemCount: 11,
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 8.0);
+          },
+          itemBuilder: (context, index) {
+            return ScheduleCard(
+              startTime: 8,
+              endTime: 9,
+              content: '프로그래밍 공부하기',
+              color: Colors.red,
+            );
+          },
+        ),
+      ),
+    );
   }
 }
